747261b4cc736e1dabcc2eb3eff73fdc148259c8
Michael <kipsang20323@gmail.com>
2019-08-24 14:45:09 -0600

    add color box styles

--- src/Palette/ColorBox.js
+++ src/Palette/ColorBox.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./ColorBox.css";
 
 const ColorBox = (props) => {
+	const { background, name } = props;
 	return (
-		<div className="box" style={{ background: props.background }}>
-			<span>{props.name}</span>
-			MORE
+		<div className="box" style={{ background }}>
+			<div className="box-content">
+				<span>{name}</span>
+			</div>
+			<button className="copy-btn">COPY</button>
+			<span class="more-btn">MORE</span>
 		</div>
 	);
 };
