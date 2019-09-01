5d0e5a2e615a3fa12156a8e045a886bada4d2540
Michael <kipsang20323@gmail.com>
2019-09-01 16:51:11 -0600

    add withStyles and start of styles for mini-palette

--- src/Palette/PaletteList.js
+++ src/Palette/PaletteList.js
@@ -1,29 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./PaletteList.css";
+import MiniPalette from "./MiniPalette";
 
 const PaletteList = (props) => {
 	const { paletteList } = props;
-	console.log(paletteList);
-
-	const paletteDisplay = paletteList.map((list) => {
-		const colors = list.colors.map((colorObj) => (
-			<div key={colorObj.name} style={{ backgroundColor: colorObj.color, height: "50px", width: "50px" }} />
-		));
-
-		return (
-			// <div className="single-palette" key={list.id}>
-			<Link className="single-palette" to={`/palette/${list.id}`} key={list.id}>
-				{colors}
-				<div className="list-name-container">
-					{list.paletteName}
-					<span>{list.emoji}</span>
-				</div>
-			</Link>
-			// </div>
-		);
-	});
-	return <div className="palette-list-container">{paletteDisplay}</div>;
+	const miniPalettes = paletteList.map((list) => <MiniPalette {...list} />);
+	return <div className="palette-list-container">{miniPalettes}</div>;
 };
 
 export default PaletteList;
